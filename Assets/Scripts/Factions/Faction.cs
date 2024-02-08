b4ca6b441cbd2d4cfdd7754347fd4fd9b770d37e
unknown <bossa2544@gmail.com>
2024-02-08 10:28:27 +0700

    Revert "prebackup"
    This reverts commit 958d4c6dbecb65f3fe4f079eb624a65c2700ec06.

--- Assets/Scripts/Factions/Faction.cs
+++ Assets/Scripts/Factions/Faction.cs
@@ -29,41 +29,6 @@ public class Faction : MonoBehaviour
     public int Stone { get { return stone; } set { stone = value; } }
     
     
-    //Headquater
-    [SerializeField] private List<Unit> aliveUnits = new List<Unit>();
-    public List<Unit> AliveUnits { get { return aliveUnits; } }
-
-    public bool CheckUnitCost(Unit unit)
-    {
-        if (food < unit.UnitCost.food)
-            return false;
-
-        if (wood < unit.UnitCost.wood)
-            return false;
-
-        if (gold < unit.UnitCost.gold)
-            return false;
-
-        if (stone < unit.UnitCost.stone)
-            return false;
-
-        return true;
-    }
-    
-    public void DeductUnitCost(Unit unit)
-    {
-        food -= unit.UnitCost.food;
-        wood -= unit.UnitCost.wood;
-        gold -= unit.UnitCost.gold;
-        stone -= unit.UnitCost.stone;
-    }
-    
-    public bool IsMyUnit(Unit u)
-    {
-        return aliveUnits.Contains(u);
-    }
-    
-    
     // Start is called before the first frame update
     void Start()
     {
