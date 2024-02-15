ee25646b1fcb91d71358724fb14a8ac82cebf509
unknown <bossa2544@gmail.com>
2024-02-15 09:08:28 +0700

    setup unit ui

--- Assets/Scripts/InfoManager.cs
+++ Assets/Scripts/InfoManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class InfoManager : MonoBehaviour
 {
-
+    [SerializeField] private Image unitPic, hpIcon, rsrcIcon, moveIcon, atkIcon, amrIcon, vslRngIcon, wpRngIcon;
+    [SerializeField] private TextMeshProUGUI nameTxt, hpTxt, rsrcTxt, moveTxt, atkTxt, amrTxt, vslRngTxt, wpRngTxt;
+    
+    
     public static InfoManager instance;
     private void Awake()
     {
