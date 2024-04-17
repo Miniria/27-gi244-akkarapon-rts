cab5233e6a857726176b9c53f1958dea50b4bb09
Miniria <bossa2544@gmail.com>
2024-04-18 00:28:18 +0700

    28-27

--- Assets/Scripts/Resources/ResourceSource.cs
+++ Assets/Scripts/Resources/ResourceSource.cs
@@ -43,7 +43,7 @@ public class ResourceSource : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        onRsrcQuantityChange.Invoke();
     }
 
     // Update is called once per frame
@@ -68,6 +68,7 @@ public class ResourceSource : MonoBehaviour
             amountToGive = amountRequest;
 
         quantity -= amountToGive;
+        onRsrcQuantityChange.Invoke();
 
         // if we're depleted, delete the resource
         if (quantity <= 0)
