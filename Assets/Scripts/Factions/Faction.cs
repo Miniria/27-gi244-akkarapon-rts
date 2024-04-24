7f4ddb82991ea11eafd7fa756a0b94d86da30c50
Miniria <bossa2544@gmail.com>
2024-04-24 19:48:07 +0700

    31-5

--- Assets/Scripts/Factions/Faction.cs
+++ Assets/Scripts/Factions/Faction.cs
@@ -9,8 +9,8 @@ public enum Nation
     Pirates,
     France,
     Spain,
-    Portuguese,
-    Dutch
+    Portugal,
+    Netherland
 }//Nation
 
 
@@ -242,7 +242,39 @@ public class Faction : MonoBehaviour
         return true;
     }
     
-    
+    public Color GetNationColor()
+    {
+        Color col;
+
+        switch(nation)
+        {
+            case Nation.Neutral:
+                col = Color.white;
+                break;
+            case Nation.Britain:
+                col = Color.red;
+                break;
+            case Nation.Pirates:
+                col = Color.black;
+                break;
+            case Nation.France:
+                col = Color.blue;
+                break;
+            case Nation.Spain:
+                col = Color.yellow;
+                break;
+            case Nation.Portugal:
+                col = Color.green;
+                break;
+            case Nation.Netherland:
+                col = new Color32 (255, 157, 0, 255);
+                break;
+            default:
+                col = Color.white;
+                break;
+        }
+        return col;
+    }
     
     
     
--- Assets/Scripts/UI/MainUi.cs
+++ Assets/Scripts/UI/MainUi.cs
@@ -23,9 +23,17 @@ public class MainUi : MonoBehaviour
     
     [SerializeField] private RectTransform selectionBox;
     public RectTransform SelectionBox { get { return selectionBox; } }
+
+    private Canvas canvas;
+    public Canvas Canvas
+    {
+        get { return canvas; }
+    }
+    
     private void Awake()
     {
-        instance = this; 
+        instance = this;
+        canvas = GetComponent<Canvas>();
     }
 
     // Start is called before the first frame update
@@ -49,4 +57,16 @@ public class MainUi : MonoBehaviour
         goldText.text = faction.Gold.ToString();
         stoneText.text = faction.Stone.ToString();
     }
+    
+    public Vector3 ScalePosition(Vector3 pos)
+    {
+        Vector3 newPos;
+
+        newPos = new Vector3(pos.x * canvas.transform.localScale.x
+            , pos.y * canvas.transform.localScale.y
+            , pos.z * canvas.transform.localScale.z);
+
+        return newPos;
+    }
+    
 }
